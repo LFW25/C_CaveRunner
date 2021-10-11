272bb6582beda325b7351a2ca20d603760da9aeb
Lily Williams <lfw25@cssecs7.canterbury.ac.nz>
2021-10-11 13:14:30 +1300

    Added the double and regular jumps

--- runner.h
+++ runner.h
@@ -13,14 +13,19 @@ uint8_t runner_crouch[] =
     0x00, 0x00, 0x00, 0x00, 0x20
 };
 
-uint8_t runner_jump[] = 
+uint8_t runner_doublejump[] = 
 {
     0x00, 0x20, 0x20, 0x00, 0x00
 };
 
+uint8_t runner_jump[] =
+{
+    0x00, 0x00, 0x20, 0x20, 0x00
+};
+
 uint8_t* runner[] = 
 {
-    runner_regular, runner_crouch, runner_jump
+    runner_regular, runner_crouch, runner_jump, runner_doublejump
 };
 
 #endif
