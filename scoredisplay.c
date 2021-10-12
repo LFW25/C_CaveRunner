a3c7c8cf35b2ee498f6df1c77cf819b8ac5e8e83
Lily Williams <lfw25@cssecs3.canterbury.ac.nz>
2021-10-13 11:58:25 +1300

    Added a little extra on modularising score display

--- scoredisplay.c
+++ scoredisplay.c
@@ -11,3 +11,20 @@ lfw25@uclive.ac.nz
 @ Date: 12 October 2021
 @ Brief: Displays the score on the pause and game-over screens
 */
+
+#include "system.h"
+#include "tinygl.h"
+#include "../../fonts/font5x7_1.h"
+#include "uint8toa.h"
+
+#include <stdbool.h>
+
+//Displays the score on pause or game over
+//Maybe move to its own module?
+void display_character (uint8_t score)
+{
+    char display_score[4];
+    
+    uint8toa(score, &display_score, true); //Convert score uint8_t to a string
+    tinygl_text (display_score);
+}
--- scoredisplay.h
+++ scoredisplay.h
@@ -14,5 +14,13 @@ lfw25@uclive.ac.nz
 #ifndef SCOREDISPLAY_H
 #define SCOREDISPLAY_H
 
+#include "system.h"
+#include "tinygl.h"
+#include "../../fonts/font5x7_1.h"
+#include "uint8toa.h"
+
+//Displays the score on pause or game over
+//Maybe move to its own module?
+void display_character (uint8_t score);
 
 #endif
