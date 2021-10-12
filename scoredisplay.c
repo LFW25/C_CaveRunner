a443f8a0ce29c24a4a397bd0ad4a43329d6a5a76
Lily Williams <lfw25@uclive.ac.nz>
2021-10-12 20:25:54 +1300

    Created scoredisplay module to modularise the pause and game over/tinygl functions

---
+++ scoredisplay.c
@@ -0,0 +1,13 @@
+/*
+@ File: Scoredisplay.c
+@ Authors:
+George Fraser
+54510750
+ghf20@uclive.ac.nz
+
+Lily Williams
+42415299
+lfw25@uclive.ac.nz
+@ Date: 12 October 2021
+@ Brief: Displays the score on the pause and game-over screens
+*/
---
+++ scoredisplay.h
@@ -0,0 +1,18 @@
+/*
+@ File: Scoredisplay.c
+@ Authors:
+George Fraser
+54510750
+ghf20@uclive.ac.nz
+
+Lily Williams
+42415299
+lfw25@uclive.ac.nz
+@ Date: 12 October 2021
+@ Brief: Displays the score on the pause and game-over screens
+*/
+#ifndef SCOREDISPLAY_H
+#define SCOREDISPLAY_H
+
+
+#endif
