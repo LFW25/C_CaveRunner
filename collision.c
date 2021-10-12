5e05e77133acf9ff722ca3b0ba35da69988763fb
Lily Williams <lfw25@cssecs7.canterbury.ac.nz>
2021-10-12 14:20:58 +1300

    Comment formatting

--- collision.c
+++ collision.c
@@ -1,15 +1,15 @@
 /*
-@ FILE: COLLISION.C
-@ AUTHORS:
-GEORGE FRASER
+@ File: Game.c
+@ Authors:
+George Fraser
 54510750
-GHF20@UCLIVE.AC.NZ
+ghf20@uclive.ac.nz
 
-LILY WILLIAMS
+Lily Williams
 42415299
-LFW25@UCLIVE.AC.NZ
-@ DATE: 12 OCTOBER 2021
-@ BRIEF: Checks if there has been a collision in the game
+lfw25@uclive.ac.nz
+@ Date: 12 October 2021
+@ Brief: Checks for collisions between runner and obstacles
 */
 
 #include "collision.h"
@@ -18,36 +18,36 @@ LFW25@UCLIVE.AC.NZ
 #include <stdbool.h>
 
 
-//checks to see if runner is in correct position for object
+//C to see if runner is in correct position for object
 bool collision_check(uint8_t runner_state, uint8_t obstacle)
 {
     switch(obstacle) {
-        case 0: //stalagtite = crouch
+        case 0: //Stalagtite = crouch
             if (runner_state == 1) {
                 return true;
             }
             break;
-        case 1: //rock = jump || super jump
+        case 1: //Rock = jump || super jump
             if (runner_state == 2 || runner_state == 3) {
                 return true;
             }
             break;
-        case 2: //bat = crouch || 
+        case 2: //Bat = crouch || 
             if (runner_state == 1 || runner_state == 3) {
                 return true;
             }
             break;
-        case 3: //boulder = super jump
+        case 3: //Boulder = super jump
             if (runner_state == 3) {
                 return true;
             }
             break;
-        case 4: //tunnel = jump
+        case 4: //Tunnel = jump
             if (runner_state == 2) {
                 return true;
             }
             break;
-        default: //will only get here if we add an object without adding a case
+        default: //Will only get here if we add an object without adding a case
             break;  
     }
     return false;
